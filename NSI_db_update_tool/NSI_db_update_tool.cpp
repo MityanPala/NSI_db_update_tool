40b4d248db4185bc64ed32250819ae9371e9d8b3
Dmitriy <69326421+MityanPala@users.noreply.github.com>
2020-08-14 00:37:48 +0300

    Almost finished list uploading to work properly with logs

--- NSI_db_update_tool/NSI_db_update_tool.cpp
+++ NSI_db_update_tool/NSI_db_update_tool.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include "curl.h"
 
+#include <stack>
+
 using namespace std;
 
 static size_t write_data(char* ptr, size_t size, size_t nmemb, string* data)
@@ -30,7 +32,7 @@ int main() // по порядку возвращаем значения сверху вниз с увеличением на 1 - ес
 	CURLcode res;
 	curl_global_init(CURL_GLOBAL_DEFAULT); // this needs to be initialized once - will allow code below to use libcurl
 
-	//ShowWindow(GetConsoleWindow(), SW_HIDE);
+	//ShowWindow(GetConsoleWindow(), SW_HIDE); // пока не отладил программу до конца - можно выводить прогресс загрузки файлов с помощью cout
 	system("mkdir all_data\n");
 	system("mkdir all_data\\configuration_and_logs\n");
 	system("mkdir all_data\\configuration_and_logs\\logs\n");
@@ -113,7 +115,7 @@ int main() // по порядку возвращаем значения сверху вниз с увеличением на 1 - ес
 	getline(extractProxyInfo, proxyString);
 	getline(extractProxyInfo, proxyUsrPwd);
 	extractProxyInfo.close();
-	if (proxyString == "" || proxyUsrPwd == "")
+	if (proxyString == "" || proxyUsrPwd == "") // нам вообще может быть не нужен прокси - тогда надо будет сделать этот return ниже, на этапе, когда не получится сделать запрос без прокси
 	{
 		writeLogs << "Cannot properly extract proxy or password from \"all_data\\configuration_and_logs\\proxy_info.txt\"" << endl;
 		writeLogs << "Please, check file structure(1st line: proxy url - \"http://xxx.xxx.xxx.xxx:xxxx\"; 2nd line - \"usr:pwd\")" << endl;
@@ -133,64 +135,58 @@ int main() // по порядку возвращаем значения сверху вниз с увеличением на 1 - ес
 		
 		urlString = urlBase + tokenString + "&page=" + pageNumber + "&size=200"; // размер выдачи больше 400 иногда не работает и сервер не хочет его обрабатывать
 
-		curl_easy_setopt(curl, CURLOPT_HTTPPROXYTUNNEL, 1L);
-		curl_easy_setopt(curl, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
-		curl_easy_setopt(curl, CURLOPT_PROXY, proxyString.c_str());
-		curl_easy_setopt(curl, CURLOPT_PROXYUSERPWD, proxyUsrPwd.c_str());
-		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
-		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-
 		res = basicRequest(urlString, curl, &content);
 		if (res != CURLE_OK)
 		{
+			writeLogs << "Request \"" << urlString << "\" failed. Cannot get response without proxy. CURLcode error: \"" << curl_easy_strerror(res) << "\"" << endl;
 
+			curl_easy_setopt(curl, CURLOPT_HTTPPROXYTUNNEL, 1L);
+			curl_easy_setopt(curl, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
+			curl_easy_setopt(curl, CURLOPT_PROXY, proxyString.c_str());
+			curl_easy_setopt(curl, CURLOPT_PROXYUSERPWD, proxyUsrPwd.c_str());
+			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
+			curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+			res = basicRequest(urlString, curl, &content);
+			if (res != CURLE_OK)
+			{
+				writeLogs << "Request \"" << urlString << "\" failed. Cannot get response using proxy. CURLcode error: \"" << curl_easy_strerror(res) << "\"" << endl;
+				writeLogs.close();
+				return 104;
+			}
 		}
 
-		cout << endl << "|" << res << endl;
-		//system("pause");
-		return 0;
+		stack <string> documentsListsNames; // запушим в стек имена файлов, в которые мы помещали списки, чтобы когда получим все списки - уже извлекать их и открывать заново
 
-		/*curl_easy_setopt(curl, CURLOPT_HTTPPROXYTUNNEL, 1L);
-		curl_easy_setopt(curl, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
-		curl_easy_setopt(curl, CURLOPT_PROXY, "10.14.10.147:8080");
-		curl_easy_setopt(curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //traffic inspector http ftp 2.0.1.721 
+		fstream documentsLists;
+		fileName = "all_data\\updated_data\\documents_lists\\" + pageNumber + "_page.txt";
+
+		documentsListsNames.push(fileName);
 		
-		curl_easy_setopt(curl, CURLOPT_PROXYUSERNAME, "dima");
-		curl_easy_setopt(curl, CURLOPT_PROXYPASSWORD, "123");
-		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
-		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);*/
-
-		/*****************************************************************/
-		
-
-		curl_easy_setopt(curl, CURLOPT_URL, urlString.c_str()); // не работает со string, надо преобразовывать в const
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content);
-
-		res = curl_easy_perform(curl); // выполнение запроса
-
-		cout << "---" << res << "---" << endl;
-		
-		if ("" == content) // при несрабатывании типа аутентификации в прокси
+		documentsLists.open(fileName.c_str(), ios::out);
+		if (!documentsLists)
 		{
-			cout << endl << content << " is empty..." << endl;
+			writeLogs << "Something went wrong while trying to save 1st list into file. Wanted destination: " << fileName << endl;
+			writeLogs.close();
+			return 105;
 		}
-		system("pause");
+		documentsLists << content;
+		documentsLists.close();
+
+		int numPos = content.find("\"total\":");
+		int numLen = content.length();
+		if (!(numPos >= 0 && numPos < numLen))
+		{
+			writeLogs << "Something went wrong while trying to get total number of documents." << fileName << endl;
+			writeLogs.close();
+			return 106;
+		}
 
 		fstream F;
-		fileName = "all_data\\updated_data\\" + pageNumber + "_page.txt";
-		F.open(fileName.c_str(), ios::out);
-		F << content;
-		F.close();
-		if (res != CURLE_OK)
-		{
-			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-		}
-		int position = content.find("\"total\":");
-		string substrInContent = content.substr(position + 8, 15);
+		string substrInContent = content.substr(numPos + 8, 15); // переделать, чтобы получать кол-во страниц выделяя тэг в json полностью по кавычкам и/или запятым-разделителям
 		//cout << endl << substrInContent << endl;
-		position = substrInContent.find(",");
-		substrInContent = substrInContent.substr(0, position); // 8 - length of "total:"
+		numPos = substrInContent.find(",");
+		substrInContent = substrInContent.substr(0, numPos); // 8 - length of "total:"
 		//cout << endl << substrInContent << endl;
 		//system("pause");
 		int convertedStringToInt = atoi(substrInContent.c_str());
@@ -209,9 +205,11 @@ int main() // по порядку возвращаем значения сверху вниз с увеличением на 1 - ес
 			maxPages++; // если к примеру в выдаче 1201 справочник, то мы получим 6 maxPages, но если разделить не нацело, то мы увидим, что надо прибавить 1 к кол-ву страниц
 		}
 
+		// оставшийся цикл для получения списков - дальше буду уже извлекать из этого списка файлов oid и загружать сами документы
 		for (int j = 2; j <= maxPages; j++)
 		{
 			pageNumber = pages[j];
+			//pageNumber = 1;
 			urlString = urlBase + tokenString + "&page=" + pageNumber + "&size=200";
 			curl_easy_setopt(curl, CURLOPT_URL, urlString.c_str());
 			//string content2;
@@ -237,10 +235,12 @@ int main() // по порядку возвращаем значения сверху вниз с увеличением на 1 - ес
 		Sleep(10000);			// ошибка при инициализации объекта curl, необходимо либо заново инициализировать,
 		cout << endl << endl;	// либо заново запустить программу - иначе искать ошибки в совместимости
 		system("pause");
-		return 0;
+		return 500;
 	}
 
-	curl_global_cleanup(); // when program doesn't use libcurl anymore in the following code
+	curl_global_cleanup();
+	writeLogs << "Program has updated database. \nNumber of successful updates: " << "uuu/xxx" << endl;
+	writeLogs << "Number of failed updates: " << "yyy/xxx" << endl;
 	writeLogs << "|end of process: " << currentDate << "|" << endl << separator << endl;
 	writeLogs.close();
 	return 0;
@@ -260,7 +260,7 @@ CURLcode basicRequest(string urlF, CURL* curlHandle, string* contentString)
 
 string replaceBadCharacters(string strToFix, const char charToReplace, const char charToReplaceWith)
 {
-	for (int i = 0; i < strToFix.length(); i++)
+	for (unsigned int i = 0; i < strToFix.length(); i++)
 	{
 		if (strToFix[i] != charToReplace)
 		{
